55b405ccf4d1967327b5e6d0e1bd95c5ad83fb72
Mila <milaivanova2609@gmail.com>
2023-01-23 11:34:32 +0200

    Completed

--- Program.cs
+++ Program.cs
@@ -32,9 +32,36 @@ namespace NetworkProgramming_ConsoleIO
                 Console.WriteLine(ex.Message);
             }
         }
+        public static void SecondTask()
+        {
+            const string ip_user = "127.0.0.1";
+            const int port = 8000;
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Parse(ip_user);
+            IPEndPoint ep = new IPEndPoint(ip, port);
+            s.Bind(ep);
+            s.Listen(15);
+            try
+            {
+                while (true)
+                {
+                    Socket ns = s.Accept();
+                    Console.WriteLine($"[{ns.RemoteEndPoint.ToString()}]: Connected");
+                    ns.Send(System.Text.Encoding.Unicode.GetBytes(DateTime.Now.ToString()));
+                    Console.ReadKey();
+                    ns.Shutdown(SocketShutdown.Both);
+                    ns.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
         static void Main()
         {
             FirstTask();
+            SecondTask();
         }
     }
 }
