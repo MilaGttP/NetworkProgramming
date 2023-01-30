a0c9563d9a931237bd3c1b4071a5936ad8792ea4
Mila <milaivanova2609@gmail.com>
2023-01-30 19:08:37 +0200

    Completed

--- Program.cs
+++ Program.cs
@@ -1,11 +1,49 @@
 ﻿
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
 namespace NetworkProgramming_ConsoleIO
 {
-    public class Program
+    public class QuoteGeneratorClient
     {
-        static void Main()
+        private const int port = 8888;
+        private static string ipUser = "127.0.0.1";
+
+        public static void Main()
         {
-            
+            Console.WriteLine("Client started...");
+            ConnectToServer();
+        }
+
+        private static void ConnectToServer()
+        {
+            IPAddress ip = IPAddress.Parse(ipUser);
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(endPoint);
+                if (socket.Connected)
+                {
+                    String strSend = "GET\r\n\r\n";
+                    socket.Send(Encoding.Unicode.GetBytes(strSend));
+                    byte[] buffer = new byte[1024];
+                    int l;
+                    do
+                    {
+                        l = socket.Receive(buffer);
+                        Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, l));
+                    } while (l > 0);
+                }
+                else Console.WriteLine("Error");
+            }
+            catch (SocketException ex) { Console.WriteLine(ex.Message); }
+            finally
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
         }
     }
 }
