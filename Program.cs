50c203d5349343a002128f0ea6516dbeaf1b54cd
Mila <milaivanova2609@gmail.com>
2023-01-31 07:43:32 +0200

    Completed

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
 ﻿
-namespace NetworkProgramming_ConsoleIO
+using System;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CurrencyRateClient
 {
-    public class Program
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            
+            Console.WriteLine("Currency Rate Client Started...");
+            Console.WriteLine("Enter two currencies (e.g. USD EUR):");
+            string currencies = Console.ReadLine();
+            TcpClient tcpClient = new TcpClient("localhost", 8888);
+            tcpClient.Connect("127.0.0.1", 8888);
+            byte[] data = Encoding.ASCII.GetBytes(currencies);
+            tcpClient.GetStream().Write(data, 0, data.Length);
+            data = new byte[1024];
+            int bytes = tcpClient.GetStream().Read(data, 0, data.Length);
+            double rate = BitConverter.ToDouble(data, 0);
+            Console.WriteLine("Exchange rate: {0}", rate);
         }
     }
 }
