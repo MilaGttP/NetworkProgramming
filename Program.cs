355cc0b83ff2432a504c71491cc133db7abf79a3
Mila <milaivanova2609@gmail.com>
2023-01-31 07:42:53 +0200

    Completed

--- Program.cs
+++ Program.cs
@@ -1,11 +1,45 @@
 ﻿
-namespace NetworkProgramming_ConsoleIO
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CurrencyRateServer
 {
-    public class Program
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 8888);
+            tcpListener.Start();
+
+            Console.WriteLine("Currency Rate Server Started...");
+            Console.WriteLine("Waiting for a connection...");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Socket socket = tcpListener.AcceptSocket();
+
+                byte[] data = new byte[1024];
+                socket.Receive(data);
+                string request = Encoding.ASCII.GetString(data);
+                double rate = GetExchangeRate(request);
+                data = BitConverter.GetBytes(rate);
+                socket.Send(data);
+                LogRequest(socket.RemoteEndPoint, request, rate);
+            }
+        }
+
+        static double GetExchangeRate(string request)
+        {
+            // TODO: Retrieve exchange rate from a database/API
+            return 1.14;
+        }
+
+        static void LogRequest(EndPoint endPoint, string request, double rate)
+        {
+            Console.WriteLine("Request from {0}: {1} - {2}", endPoint, request, rate);
         }
     }
 }
